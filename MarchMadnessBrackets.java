e76541760655a44b231999bb5f7ee4eb35a478c6
felix1429 <thennig314@gmail.com>
2014-03-25 21:29:39 -0500

    worked on brackets

--- MarchMadnessBrackets.java
+++ MarchMadnessBrackets.java
@@ -4,22 +4,56 @@ import java.util.*;
 
 public class MarchMadnessBrackets {
 	
+	public static Bracket makeBracket(Character type) {
+		Bracket aBracket = new Bracket(type.toString());
+		return aBracket;
+	}
+	
 	public static void main(String[] args) {
 		
-		ArrayList<Bracket> bracketList = new ArrayList<Bracket>();
+		Stack<Bracket> bracketStack = new Stack<Bracket>();
 		String wholeInput;
+		String blockString;
+		String finalString = "";
+		Bracket bracketVar;
+		Character iterVar;
 		Scanner sc = new Scanner(System.in);
-		String scanInput;
 		
-		System.out.println("Input a string");
-		wholeInput = sc.nextLine();
-		char[] inputCharArray = wholeInput.toCharArray();
-		int counter = 0;
-		for(Character chr : inputCharArray) {
-			if(chr.equals('(') || chr.equals('{') || chr.equals('[')) {
-				Bracket aBracket = new Bracket(chr.toString(), "");
-				bracketList.add(aBracket);
+		try {
+			System.out.println("Input a string");
+
+			wholeInput = sc.nextLine();
+			char[] inputCharArray = wholeInput.toCharArray();
+			List<Character> wholeList = new ArrayList<Character>();
+			for(Character a : inputCharArray) {
+				wholeList.add(a);
 			}
+			blockString = "";
+			Iterator<Character> iter = wholeList.iterator();
+			bracketStack.push(makeBracket(iter.next()));
+			while(iter.hasNext()) {
+				iterVar = iter.next();
+				if(iterVar.equals('(') || iterVar.equals('{') || iterVar.equals('[')) {
+					if(!(blockString.equals(""))) {
+						bracketVar = makeBracket(iterVar);
+						bracketVar.content = blockString.toString();
+						bracketStack.push(bracketVar);
+						blockString = "";
+					}else {
+						blockString = "";
+					}	
+				}else if(iterVar.equals(')') || iterVar.equals('}') || iterVar.equals(']')) {
+					bracketVar = bracketStack.pop();
+					finalString = bracketVar.content.concat((blockString.toString()));
+					System.out.println(finalString);
+					finalString = "";
+				}else {
+					blockString.concat(iterVar.toString());
+				}
+				
+			}
+		}finally {
+			sc.close();
 		}
 	}
 	
@@ -28,8 +62,12 @@ public class MarchMadnessBrackets {
 		public String bracketType;
 		public String content = "";
 		
-		public Bracket(String startBracketType, String startContent) {
+		public Bracket(String startBracketType) {
 			this.bracketType = startBracketType;
+			this.content = "";
+		}
+		
+		public void addContent(String startContent) {
 			this.content = startContent;
 		}
 		
